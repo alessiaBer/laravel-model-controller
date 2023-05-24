c5c00662ad6d49d7d7a9aa97229df16c7fb4ce32
alessiaBer <alexdeyn@gmail.com>
2023-05-24 16:17:35 +0200

    [add] some style to cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,29 +16,32 @@
 </head>
 
 <body>
-    <div class="container">
-
-        <h1>My MovieDB</h1>
-
-        <div class="row row-cols-1 row-cols-md-3 row-cols-lg-5">
-            @foreach($movies as $movie)
-            <div class="col">
-                <div class="card">
-                    <div class="card-img">
-                        <img src="{{$movie->poster_img}}" alt="{{$movie->title}}" class="card-img-top">
-                    </div>
-                    <div class="card-body">
-                        <h6>{{$movie->title}}</h6>
-                        <p>{{$movie->original_title}}</p>
-                        <span class="d-block">Nationality: {{$movie->nationality}}</span>
-                        <span class="d-block">Vote: {{$movie->vote}}</span>
-                        <span class="d-block">Date: {{$movie->date}}</span>
+    <main class="bg-dark text-white">
+        <div class="container">
+    
+            <h1 class="py-4">My MovieDB</h1>
+    
+            <div class="row row-cols-1 row-cols-md-3 row-cols-lg-5 pb-5">
+                @foreach($movies as $movie)
+                <div class="col">
+                    <div class="card bg-transparent">
+                        <div class="card-img">
+                            <img src="{{$movie->poster_img}}" alt="{{$movie->title}}" class="card-img-top">
+                        </div>
+                        <div class="card-body">
+                            <h6>{{$movie->title}}</h6>
+                            <p>{{$movie->original_title}}</p>
+                            <span class="d-block">Nationality: {{$movie->nationality}}</span>
+                            <span class="d-block">Vote: {{$movie->vote}}</span>
+                            <span class="d-block">Date: {{$movie->date}}</span>
+                        </div>
                     </div>
                 </div>
+                @endforeach
             </div>
-            @endforeach
         </div>
-    </div>
+
+    </main>
 
 </body>
 
